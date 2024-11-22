f48221d10cf62f5d9f6884c0b568cf258fdc602e
Tanishk Sonani <100336884+TanishkSonani@users.noreply.github.com>
2024-11-22 13:53:44 +0530

    initial commit

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_LTX_Task_FILE D:/c++/Projects/LTX_Task/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/LTX_Task.exe)
+set(__QT_DEPLOY_TARGET_LTX_Task_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
