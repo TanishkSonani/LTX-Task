4cff2dc868d2a6960ce4a1660d721463b5551a2a
Tanishk Sonani <100336884+TanishkSonani@users.noreply.github.com>
2024-11-23 12:58:16 +0530

    csv file opened and logged output into terminal

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,7 +1,32 @@
 #include "mainwindow.h"
+#include <CsvParser.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    QString buildDir = QDir::currentPath(); //cwd
+    // qDebug() << buildDir;
+
+    QDir dir(buildDir);
+    dir.cdUp(); //build dir
+    dir.cdUp(); //project dir
+    QString projectDir = dir.absolutePath();
+    QString filePath =  QDir(projectDir).filePath("data/NZ5_flightdatar.csv");
+
+    // qDebug() << filePath;
+
+    QFile CSVFile(filePath);
+    if (CSVFile.open(QIODevice::ReadOnly))
+    {
+        QTextStream Stream(&CSVFile);
+        while (Stream.atEnd() == false)
+        {
+            QString LineData = Stream.readLine();
+            qDebug() << LineData;
+        }
+    }
+
+    CSVFile.close();
+}
 
 MainWindow::~MainWindow() {}
